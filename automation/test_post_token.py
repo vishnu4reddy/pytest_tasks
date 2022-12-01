26113bf99f9d04d25bac102e82f14f0ba9dca1c1
vishnu4reddy <vreddy@msystechnologies.com>
2022-12-01 15:43:35 +0530

    test cases

--- automation/test_post_token.py
+++ automation/test_post_token.py
@@ -4,14 +4,14 @@ payload={
     "email": "eve.holt@reqres.in",
     "password": "pistol"
 }
-# def testpost():
-resp = requests.post("https://reqres.in/api/register",data=payload)
-data = json.loads(open("data.json","r").read())
-print(resp.content)
-print(resp.json()["id"])
-assert resp.json()['token']== 'QpwL5tke4Pnpja7X4'
-# assert resp.json()["password"]== "pistol"
-# assert resp["data"]["email"]== "pistol"
-# assert 
-print(data["email"])
-assert (data["email"]) == "eve.holt@reqres.in"
+def testpost():
+    resp = requests.post("https://reqres.in/api/register",data=payload)
+    data = json.loads(open("data.json","r").read())
+    print(resp.content)
+    print(resp.json()["id"])
+    assert resp.json()['token']== 'QpwL5tke4Pnpja7X4'
+    # assert resp.json()["password"]== "pistol"
+    # assert resp["data"]["email"]== "pistol"
+    # assert 
+    print(data["email"])
+    assert (data["email"]) == "eve.holt@reqres.in"
